cf747e3a9ee24c6306c9fd54e5edf2468cc36a55
Omri Zaiman <omri162004@gmail.com>
2021-05-13 20:11:36 +0300

    added documentation in ClientSocket class

--- Client/ClientSocket.cpp
+++ Client/ClientSocket.cpp
@@ -1,32 +1,48 @@
 //
-// Created by omrizaiman on 12/05/2021.
+// Created by Omri Zaiman on 12/05/2021.
 //
 
 #include "ClientSocket.h"
 #include "SocketException.h"
 
-
+/*
+A constructor of a ClientSocket object. It creates a new socket and bind it
+input: the ip and port of the server
+output: a new object of ClientSocket class (a conversation socket)
+*/
 ClientSocket::ClientSocket ( std::string host, int port )
 {
     Socket::create();
-    Socket::connect ( host, port );
+    Socket::connect (host, port);
 }
 
+/*
+The method is a sending operator to send data to a socket
+input: the vector of bytes to send through the socket
+output: a reference to this ClientSocket object
+*/
 const ClientSocket& ClientSocket::operator << ( const vector<byte>& vec ) const
 {
-    //std::cout << "vec[0]: " << (int)vec[0] << std::endl;
-    //std::cout << "size: " << vec.size() << std::endl;
     Socket::send (vec);
     return *this;
 }
 
+/*
+The method is a sending operator to send data to a socket
+input: the string to send through the socket
+output: a reference to this ClientSocket object
+*/
 const ClientSocket& ClientSocket::operator << ( const std::string& s ) const
 {
     Socket::send ( s );
     return *this;
 }
 
-
+/*
+The method is a receiving operator to get data from a socket
+input: the string to fill in with data
+output: a reference to this ClientSocket object
+*/
 const ClientSocket& ClientSocket::operator >> ( std::string& s ) const
 {
     if ( ! Socket::recv ( s ) )
